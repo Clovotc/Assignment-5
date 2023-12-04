b6e194dddc9adc53f3734f0f289f7fbed9fd250f
Maison Kasprick <maison.kasprick@ndus.edu>
2023-12-04 10:51:39 -0600

    Added the song class, created the song controller, and generated the song db. Changed the main page to display Music Store instead of Assignment 5

---
+++ Models/Song.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_5.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Singer { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
